61a3375600924a689e970634a9e7aa8b01d2d51a
Hyeonil Na <sjrnfl528@gmail.com>
2021-11-29 19:46:49 +0900

    11월 29일 수정

--- mydjango11/hyeonilapp/models.py
+++ mydjango11/hyeonilapp/models.py
@@ -2,6 +2,6 @@ from django.db import models
 
 class hyeonil(models.Model):
     gamename = models.CharField(max_length=100, verbose_name="게임명")
-    gamedate = models.DateField(verbose_name="플레이 날짜")
+    gamedate = models.DateTimeField(verbose_name="플레이 날짜")
     clear = models.BooleanField(verbose_name="클리어 여부")
     gold = models.CharField(max_length=100, verbose_name="획득골드")
