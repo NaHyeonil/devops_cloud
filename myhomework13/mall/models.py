c4dca4e87a92a5fa0d6b1d19d29070bd529a9b1c
Hyeonil Na <sjrnfl528@gmail.com>
2021-12-01 17:58:29 +0900

    mall/Shop에 telephone2 필드 추가

--- myhomework13/mall/models.py
+++ myhomework13/mall/models.py
@@ -4,6 +4,7 @@ class Shop(models.Model):
     name = models.CharField(max_length=100)
     description = models.TextField(blank=True)
     telephone = models.CharField(max_length=13)
+    telephone2 = models.CharField(max_length=13)
     photo = models.ImageField(blank=True)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
