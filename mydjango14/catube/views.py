99f17e446922d19c75dfcf1cea509af77b3611a6
Hyeonil Na <sjrnfl528@gmail.com>
2021-12-02 15:47:13 +0900

    CBV

--- mydjango14/catube/views.py
+++ mydjango14/catube/views.py
@@ -1,8 +1,13 @@
 from django.http import HttpRequest, HttpResponse
 from django.shortcuts import render
+from django.views.generic import ListView
+
 from catube.models import Video
 
+# Class Based View (CBV)
+# index = ListView.as_view(model=Video, template_name="catube/index.html")
 
+# Function Based View (FBV)
 def index(request: HttpRequest) -> HttpResponse:
     qs = Video.objects.all()
     return render(
@@ -10,5 +15,16 @@ def index(request: HttpRequest) -> HttpResponse:
         "catube/index.html",
         {
             "video_list": qs,
+        },
+    )
+
+
+def video_detail(request: HttpRequest, pk: int) -> HttpResponse:
+    video = Video.objects.get(pk=pk)
+    return render(
+        request,
+        "catube/video_detail.html",
+        {
+            "video": video,
         },
     )
