efebea2b06c1297ca77fd22ef9a39c83963f319d
Hyeonil Na <sjrnfl528@gmail.com>
2021-12-08 14:58:08 +0900

    story 앱 생성 및 models 작성

---
+++ mydjango18/story/models.py
@@ -0,0 +1,42 @@
+from django.db import models
+
+
+class Timestamp(models.Model):
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        abstract = True
+
+
+class Storyboard(Timestamp):
+    author_name = models.CharField(max_length=20)
+    title = models.CharField(max_length=200, db_index=True)
+    content = models.TextField()
+
+    def __str__(self) -> str:
+        return self.title
+
+    class Meta:
+        verbose_name = "창작 이야기"
+        verbose_name_plural = "이야기 목록"
+
+
+class Comment(Timestamp):
+    author_name = models.CharField(max_length=20)
+    content = models.TextField()
+
+    class Meta:
+        verbose_name = "이야기 댓글"
+        verbose_name_plural = "댓글 목록"
+
+
+class Tag(Timestamp):
+    name = models.CharField(max_length=200)
+
+    def __str__(self) -> str:
+        return self.name
+
+    class Meta:
+        verbose_name = "태그"
+        verbose_name_plural = "태그 목록"
