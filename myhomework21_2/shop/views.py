a6bb54ae500a772455232a0976d0ee2ed00051e2
Hyeonil Na <sjrnfl528@gmail.com>
2021-12-14 01:50:33 +0900

    html 생성

--- myhomework21_2/shop/views.py
+++ myhomework21_2/shop/views.py
@@ -1,3 +1,18 @@
-from django.shortcuts import render
+from django.http import HttpRequest, HttpResponse
+from django.shortcuts import render, redirect, get_object_or_404
 
-# Create your views here.
+
+def shop_list(request: HttpRequest) -> HttpResponse:
+    return render(request, "shop/shop_list.html")
+
+
+def shop_new(request: HttpRequest) -> HttpResponse:
+    return render(request, "shop/shop_form.html")
+
+
+def shop_detail(request: HttpRequest) -> HttpResponse:
+    return render(request, "shop/shop_detail.html")
+
+
+def shop_edit(request: HttpRequest) -> HttpResponse:
+    return render(request, "shop/shop_detail.html")
