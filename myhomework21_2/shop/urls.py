d70604d6e04d79ae2921cd5cac27b08955196ebc
Hyeonil Na <sjrnfl528@gmail.com>
2021-12-14 02:52:35 +0900

    shop_list 템플릿에서 간단 검색 구현까지

--- myhomework21_2/shop/urls.py
+++ myhomework21_2/shop/urls.py
@@ -1,1 +1,13 @@
-urlpatterns = []
+from django.urls import path
+
+from shop import views
+
+app_name = "shop"
+
+
+urlpatterns = [
+    path("", views.shop_list, name="shop_list"),
+    path("new/", views.shop_new, name="shop_new"),
+    path("<int:pk>/", views.shop_detail, name="shop_detail"),
+    path("<int:pk>/edit/", views.shop_edit, name="shop_edit"),
+]
--- myhomework21_2/shop/views.py
+++ myhomework21_2/shop/views.py
@@ -1,18 +1,64 @@
 from django.http import HttpRequest, HttpResponse
 from django.shortcuts import render, redirect, get_object_or_404
 
+from shop.forms import ShopForm
+from shop.models import Shop, Category, Tag
 
 def shop_list(request: HttpRequest) -> HttpResponse:
-    return render(request, "shop/shop_list.html")
+    category_qs = Category.objects.all()
+    qs = Shop.objects.all() #.order_by("-id")
+
+    category_id: str = request.GET.get("category_id", "")
+    if category_id:
+        qs = qs.filter(category__id=category_id)
+
+    query = request.GET.get("query", "")
+    if query:
+        qs = qs.filter(name__icontains=query)
+
+    return render(request, "shop/shop_list.html",{
+        "category_list": category_qs,
+        "shop_list":qs,
+    })
 
 
 def shop_new(request: HttpRequest) -> HttpResponse:
-    return render(request, "shop/shop_form.html")
+    # raise NotImplementedError("곧 구현 예정")
+
+    if request.method == "POST":
+        form = ShopForm(request.POST, request.FILES)
+        if form.is_valid():
+            saved_post = form.save()
 
 
-def shop_detail(request: HttpRequest) -> HttpResponse:
-    return render(request, "shop/shop_detail.html")
+            # shop_detail 뷰를 구현했다면 !!!
+            return redirect("shop:shop_detail", saved_post.pk)
+    else:
+        form = ShopForm()
+
+    return render(request, "shop/shop_form.html", {
+        "form": form,
+    })
 
 
-def shop_edit(request: HttpRequest) -> HttpResponse:
-    return render(request, "shop/shop_detail.html")
+def shop_detail(request: HttpRequest, pk: int) -> HttpResponse:
+    shop = get_object_or_404(Shop, pk=pk)
+    return render(request, "shop/shop_detail.html", {
+        "shop": shop,
+    })
+
+
+def shop_edit(request: HttpRequest, pk: int) -> HttpResponse:
+    shop = get_object_or_404(Shop, pk=pk)
+
+    if request.method == "POST":
+        form = ShopForm(request.POST, request.FILES, instance=shop)
+        if form.is_valid():
+            saved_shop = form.save()
+            return redirect("shop:shop_detail", saved_shop.pk)
+    else:
+        form = ShopForm(instance=shop)
+
+    return render(request, "shop/shop_form.html", {
+        "form": form,
+    })
