4869fd29e2c0a8bca5f1a6c197648894738256dd
Hyeonil Na <sjrnfl528@gmail.com>
2021-12-14 01:32:05 +0900

    models 작성 및 admin 등록

--- myhomework21_2/shop/models.py
+++ myhomework21_2/shop/models.py
@@ -1,2 +1,30 @@
+from django.core.validators import RegexValidator
 from django.db import models
 
+
+class TimestampedModel(models.Model):
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        abstract = True
+
+
+class Category(TimestampedModel):
+    name = models.CharField(max_length=100, unique=True)
+
+
+class Shop(TimestampedModel):
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+    name = models.CharField(max_length=100, db_index=True)
+    description = models.TextField(blank=True)
+    telephone = models.CharField(max_length=14,
+                                 validators=[
+                                     RegexValidator(r"^\d{3}-?\d{4}-?\d{4}$", message = "전화번호를 입력해주세요.")
+                                 ],
+                                 help_text="입력예) 010-0000-0000")
+    tag_set = models.ManyToManyField("Tag", blank=True)
+
+
+class Tag(TimestampedModel):
+    name = models.CharField(max_length=100, unique=True)
