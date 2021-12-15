b111c45b4a1bf9e35284214ddc46d9bf9985a167
Hyeonil Na <sjrnfl528@gmail.com>
2021-12-15 10:43:01 +0900

    blog앱 생성 및 등록

---
+++ mydjango23/blog/models.py
@@ -0,0 +1,18 @@
+from django.db import models
+
+class TimestampedModel(models.Model):
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        abstract = True
+
+
+class Category(TimestampedModel):
+    name = models.CharField(max_length=100, unique=True)
+
+    def __str__(self) -> str:
+        return self.name
+
+    class Meta:
+        ordering = ["-id"]
