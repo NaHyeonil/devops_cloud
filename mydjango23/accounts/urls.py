8dba83656385193ba6dde11a37570ab9bfbf3c68
Hyeonil Na <sjrnfl528@gmail.com>
2021-12-16 16:04:53 +0900

    login/profile/logout 뷰 구현

--- mydjango23/accounts/urls.py
+++ mydjango23/accounts/urls.py
@@ -8,7 +8,7 @@ app_name = "accounts"
 urlpatterns = [
     path("profile.png", views.profile_image, name="profile_image"),
     path("login/", views.login, name="login"),
-    path("signup", views.signup, name="signup"),
-    path("profile", views.profile, name="profile"),
-    path("logout", views.logout, name="logout"),
+    path("signup/", views.signup, name="signup"),
+    path("profile/", views.profile, name="profile"),
+    path("logout/", views.logout, name="logout"),
 ]
