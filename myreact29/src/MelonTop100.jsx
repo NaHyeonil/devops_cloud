7887743896cf2a40f91546b98dfc20098c8ae949
Hyeonil Na <sjrnfl528@gmail.com>
2021-12-23 14:27:16 +0900

    myreact29

---
+++ myreact29/src/MelonTop100.jsx
@@ -0,0 +1,31 @@
+import { useState } from 'react';
+import { Button } from 'react-bootstrap';
+import initialSongList from './data/melon_data.json';
+import './MelonTop100.css';
+
+// 바뀌지 않는 데이터
+// 초기값을 정의
+
+function MelonTop100() {
+  const [songList, setSongList] = useState([]);
+  const handleClick = () => {
+    setSongList(initialSongList);
+  };
+  return (
+    <div>
+      <h2>멜론 top 100</h2>
+      <Button onClick={handleClick}>로딩</Button>
+      <ul className="songList">
+        {songList.map((song) => {
+          return (
+            <li>
+              [{song.rank}] {song.title} by {song.artist}
+            </li>
+          );
+        })}
+      </ul>
+    </div>
+  );
+}
+
+export default MelonTop100;
